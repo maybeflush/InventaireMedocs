0ad3698783ae21ee50f2d91146c273251089ae21
maybeflush <maybeflush@users.noreply.github.com>
2025-10-23 19:24:38 +0000

    deploy: 6e12b908b45daa93a3801a2bfebca7a193f57368

---
+++ env.js
@@ -0,0 +1,4 @@
+// Generated at deploy-time by GitHub Actions.
+// Do not commit this file to source control.
+window.__ENV = window.__ENV || {};
+window.__ENV.FIREBASE_CONFIG = JSON.parse(`$FIREBASE_CONFIG_JSON`);
